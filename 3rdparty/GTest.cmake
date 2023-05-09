919c5a301bec273c9668c6e9d9622f28ed83068a
Vadim Piven <vadim@piven.tech>
2023-05-09 17:14:42 +0400

    Configure CI (#22)
    * Add 3rdparty notices
    
    * Use vcpkg

--- 3rdparty/GTest.cmake
+++
@@ -1,8 +0,0 @@
-FetchContent_Declare(googletest
-    # b796f7d44681514f58a683a3a71ff17c94edb0c1 is a commit hash for tag v1.13.0
-    URL https://github.com/google/googletest/archive/b796f7d44681514f58a683a3a71ff17c94edb0c1.zip
-)
-set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-FetchContent_MakeAvailable(googletest)
-enable_testing()
-include(GoogleTest)
