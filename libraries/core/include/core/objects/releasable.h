2cc2a9e12011ef28695e26d18e7d73dd63613165
Vadim Piven <vadim@piven.tech>
2023-05-09 13:26:52 +0400

    MacOS implementation refined

--- libraries/core/include/core/objects/releasable.h
+++ libraries/core/include/core/objects/releasable.h
@@ -4,8 +4,7 @@
 
 #include <concepts>
 #include <functional>
-#include <stdexcept>
-#include <string>
+#include <optional>
 #include <utility>
 
 #include <core_export.h>
@@ -13,7 +12,7 @@
 namespace core {
 
 /// Use std::unique_ptr with custom deleter instead whenever possible
-template <typename T, typename Releaser = std::function<void(T)>, T Invalid = T()>
+template <typename T, typename Releaser = std::function<void(T)>>
     requires std::destructible<T>
           && std::is_nothrow_copy_constructible_v<T>
           && std::is_nothrow_move_constructible_v<T>
@@ -22,64 +21,58 @@ template <typename T, typename Releaser = std::function<void(T)>, T Invalid = T(
 class CORE_EXPORT Releasable : public NonCopiable
 {
 public:
-    constexpr Releasable()
-        : m_release()
-        , m_value(Invalid)
-        , m_invalid(Invalid)
-    {}
+    constexpr Releasable() = default;
 
-    Releasable(T && value, Releaser && release, T && invalid = Invalid)
-        : m_release(std::move(release))
-        , m_value(std::move(value))
-        , m_invalid(std::move(invalid))
-    {}
+    Releasable(T && value, T && invalid, Releaser && release)
+        : m_value(std::move(value))
+        , m_release(std::move(release))
+    {
+        if (m_value.value() == invalid)
+        {
+            [[unlikely]]
+            m_value.reset();
+        }
+    }
 
     constexpr Releasable(Releasable && other) noexcept
-        : m_release(std::move(other.m_release))
-        , m_value(std::move(other.m_value))
-        , m_invalid(std::move(other.invalid))
+        : m_value(std::move(other.m_value))
+        , m_release(std::move(other.m_release))
     {}
 
     constexpr Releasable & operator=(Releasable && other) noexcept
     {
         if (this == &other)
+        {
+            [[unlikely]]
             return *this;
+        }
 
-        std::swap(m_release, other.m_release);
         std::swap(m_value, other.m_value);
-        std::swap(m_invalid, other.m_invalid);
+        std::swap(m_release, other.m_release);
         return *this;
     }
 
-    [[nodiscard]] constexpr bool Initialized() const noexcept
+    [[nodiscard]] constexpr bool operator !() const noexcept
     {
-        return m_value != m_invalid;
-    }
-
-    [[nodiscard]] constexpr explicit operator bool() const noexcept
-    {
-        return Initialized();
+        return !m_value.has_value();
     }
 
     [[nodiscard]] T operator *() const
     {
-        if (!Initialized())
-        {
-            throw std::logic_error(__func__ + std::string(" not initialized"));
-        }
-        return m_value;
+        return m_value.value();
     }
 
     void Release() noexcept
     {
-        if (Initialized())
+        if (m_value.has_value())
         {
+            [[likely]]
             try
             {
-                (void) std::invoke(m_release, m_value);
+                (void) std::invoke(m_release, m_value.value());
             }
             catch (...) {}
-            m_value = m_invalid;
+            m_value.reset();
         }
     }
 
@@ -89,9 +82,8 @@ public:
     }
 
 private:
+    std::optional<T> m_value;
     Releaser m_release;
-    T m_value;
-    T m_invalid;
 };
 
 } // namespace core
